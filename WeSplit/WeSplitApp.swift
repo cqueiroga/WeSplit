b8bf55f49036fdc36f618916eaca085a27c1e4e7
Chris Queiroga <blu3shock3r@gmail.com>
2022-11-30 19:02:37 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Chris Queiroga on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
